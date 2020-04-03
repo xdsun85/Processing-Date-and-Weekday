d465093b39a31c06d51034da7db3dfc7e4a20ae3
xdsun85 <49748886+xdsun85@users.noreply.github.com>
2020-04-03 17:29:29 +0800

    Add files via upload

--- week.py
+++ week.py
@@ -13,7 +13,7 @@ def is_leap_year(year):
 def parse(date):
     date = date.replace('-', '').replace('_', '')
     if len(date) != 8:
-        print('Invalid date!')
+        print('Invalid date %s.' % date)
         return -1, -1, -1
 
     yyyy = int(date[:4])
@@ -28,7 +28,7 @@ def parse(date):
     if mm <= 12 and dd <= month_days[mm - 1]:
         return yyyy, mm, dd
     else:
-        print('Invalid date!')
+        print('Invalid date %s.' % date)
         return -1, -1, -1
 
 # return the index of a certain date in that year
@@ -59,7 +59,7 @@ def index2date(yyyy, idx):
         month_days_cum = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
     if idx <= 0 or idx > month_days_cum[-1]:
-        return 'invalid index!', -1, -1, -1
+        return 'invalid index %s.' % idx, -1, -1, -1
 
     for i in range(1, 13):
         if idx <= month_days_cum[i]:
@@ -105,7 +105,7 @@ def date2day(date):
 # 2020, 9, 'Sat' -> 60 which means the 60th date (Feb 29th) in 2020
 def day2index(yyyy, n, target='Sun'):
     if n <= 0 or n > 53:
-        return 'Invalid num!'
+        return 'Invalid num %s.' % n
 
     new_year_day = date2day(str(yyyy) + '0101')  # the day of the New Year's Day
     day_dict = {'Sun': 0, 'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5, 'Sat': 6}
@@ -131,8 +131,56 @@ def date2week(date, start='Sun'):
 
     return  week_in_year
 
+# return the number of leap years between two different years (borders are NOT counted)
+# e.g 2020, 2021 -> 0; 2020, 2030 -> 2; 2016, 2020 -> 0; 2021, 2019 -> 1
+def n_leaps(yyyy1, yyyy2):
+    if yyyy1 > yyyy2:
+        yyyy1, yyyy2 = yyyy2, yyyy1
+
+    n = 0
+    for y in range(yyyy1 + 1, yyyy2):
+        if is_leap_year(y):
+            n += 1
+            y += 4
+
+    return n
+
+# return the interval between 2 dates
+# e.g. '20190227', 20200403' -> 401; 20200403', '20200101' -> -91
+def dates_interval(date1, date2):
+    yyyy1, mm1, dd1 = parse(date1)
+    yyyy2, mm2, dd2 = parse(date2)
+    if -1 in (yyyy1, mm1, dd1, yyyy2, mm2, dd2):
+        return float('inf')
+
+    date_in_year1 = date2index(date1)
+    date_in_year2 = date2index(date2)
+
+    if yyyy1 == yyyy2:
+        return date_in_year2 - date_in_year1
+    elif yyyy1 < yyyy2:
+        if is_leap_year(yyyy1):
+            interval1 = 366 - date_in_year1
+        else:
+            interval1 = 365 - date_in_year1
+        interval12 = (yyyy2 - yyyy1 - 1) * 365 + n_leaps(yyyy1, yyyy2)
+        interval2 = date_in_year2
+
+        return interval1 + interval12 + interval2
+    else:
+        if is_leap_year(yyyy2):
+            interval2 = 366 - date_in_year2
+        else:
+            interval2 = 365 - date_in_year2
+        interval21 = (yyyy1 - yyyy2 - 1) * 365 + n_leaps(yyyy2, yyyy1)
+        interval1 = date_in_year1
+
+        return -interval2 - interval21 - interval1
+
 print(date2index('20200402'))              # 93
 print(index2date(2020, 94))                  # ('20200403', 2020, 4, 3)
 print(date2day('20200402'))                 # 4
 print(day2index(2020, 14, 'Fri'))           # 94
 print(date2week('20200402', 'Mon'))  # 14
+print(n_leaps(2011, 2020))                   # 2
+print(dates_interval('20110902', '20160710'))   # 1773
